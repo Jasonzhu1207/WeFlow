5cec0efc1ec6f559c4784ae9f5a6112cc454f323
Jason <jasonzhu081207@gmail.com>
2026-05-01 12:32:42 +0800

    fix(ui): icon

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-/** Merge Tailwind classes with clsx for conditional + deduplication */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
